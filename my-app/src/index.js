58c998a1413340aa21cad0a9e0b55e07c3c43362
StephenH <StephenHwang156@outlook.com>
2019-03-09 18:05:59 -0500

    for tiffany

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+    render() {
+        return <h1>
+            Hello World
+        </h1>
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
